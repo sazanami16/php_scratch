de80f74b1db238e73ad74f862f145ca782580fe5
sazanami16 <za_nami.za_nami16@icloud.com>
2022-08-16 18:41:33 +0900

    staff edit and delete system

--- staff/staff_list.php
+++ staff/staff_list.php
@@ -24,7 +24,7 @@
 
 		print 'スタッフ一覧 <br /><br />';
 
-		print '<form method="post" action="staff_edit.php">';
+		print '<form method="post" action="staff_branch.php">';
 		while (true) {
 			$rec = $stmt->fetch(PDO::FETCH_ASSOC);
 			if ($rec == false) {
@@ -35,7 +35,8 @@
 			print '<br />';
 		}
 
-		print '<input type="submit" value="修正">';
+		print '<input type="submit" name="edit" value="修正">';
+		print '<input type="submit" name="delete" value="削除">';
 		print '</form>';
 	} catch (Exception $e) {
 		print 'ただいま障害により大変ご迷惑をおかけしています。';
